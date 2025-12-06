7ee3aeb7e6c63f4c4958682f23b555d254067f11
Ahamid047 <ahamid234567890@gmail.com>
2025-12-07 00:13:22 +0530

    Remove unused code and format the file

--- hello.java
+++ hello.java
@@ -1,41 +1,25 @@
 import java.util.Arrays;
-
 class hello {
     public static void main(String[] args) {
-        int[] arr = {3,5,6,3};
-        int [] fresult= productExceptSelf(arr);
+        int[] arr = {3, 5, 6, 3};
+        int[] fresult = productExceptSelf(arr);
         System.out.print(Arrays.toString(fresult));
-
-        // System.out.println(fresult);
     }
 
     static int[] productExceptSelf(int[] nums) {
         int tpod = 1;
         int[] res = new int[nums.length];
-
-        int j=0;
-        for (int num =0;num<nums.length;num++) {
+        int j = 0;
+        for (int num = 0; num < nums.length; num++) {
             res[num] = tpod;
-            // System.out.println(num);
             tpod *= nums[num];
-
         }
-        int lpod =1;
-        // System.out.println(nums.length);
-        for (int i = nums.length-1; i >= 0; i--) {
 
+        int lpod = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
             res[i] *= lpod;
-
-            lpod *= nums[i]; 
-
-
-            // res[i] = tpod / nums[i];
-            // System.out.println(res[i]);
-
+            lpod *= nums[i];
         }
-
         return res;
     }
-
 }
-// [1,1,2,3]
