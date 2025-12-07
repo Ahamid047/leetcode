1c85d87cb92bfe6b6fdc4a2546f3a2126606f8ee
Ahamid047 <ahamid234567890@gmail.com>
2025-12-07 14:44:43 +0530

    Largest prime number equal to sum of previous consecutive prime numbers

---
+++ largest_prime.java
@@ -0,0 +1,30 @@
+class LargestPrime {
+
+    public static boolean isPrime(int x) {
+        if (x <= 1) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int largestPrime(int n) {
+        int sum = 0;
+        int answer = 0;
+
+        for (int num = 2; num <= n; num++) {
+            if (isPrime(num)) {
+                sum += num;
+                if (sum > n) break;
+                if (isPrime(sum)) answer = sum;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(largestPrime(n));
+    }
+}
