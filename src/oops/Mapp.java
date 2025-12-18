91f607d89efce0760c434e0fb5dc0f7570801992
Ahamid047 <ahamid234567890@gmail.com>
2025-12-18 17:09:42 +0530

    Recursion basics

---
+++ src/oops/Mapp.java
@@ -0,0 +1,33 @@
+package oops;
+
+import java.util.HashSet;
+
+public class Mapp {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        // map.put(2,"Hamid");
+    
+        System.out.println(set);
+        
+    }
+    
+}
+
+
+// package oops;
+
+// import java.util.HashMap;
+
+// public class Mapp {
+//     public static void main(String[] args) {
+//         HashMap<Integer, String> map = new HashMap<>();
+//         map.put(1,"Abdul");
+//         map.put(2,"Hamid");
+    
+//         System.out.println(map);
+        
+//     }
+    
+// }
