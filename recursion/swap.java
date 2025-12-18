c4eef8b93b76a7f2133cebcdcd0c7007e7defb4d
Ahamid047 <ahamid234567890@gmail.com>
2025-12-18 18:40:11 +0530

    swapping the array elemnt through recursion

--- recursion/swap.java
+++ recursion/swap.java
@@ -9,16 +9,26 @@ public class swap {
         for(int i =0;i<arr.length;i++){
             arr[i]= sc.nextInt();
         }
+        swap(arr, 0,n-1);
         for (int i = 0; i < n; i++) {
-             System.out.print(arr[i]);
+             System.out.print(arr[i]+" ");
             
         }
 
         
 
     }
-    // public static void swap(arr){
+    public static void swap(int []arr, int left, int right){
+        if(left >= right){
+            return;
+        }
+        int temp = arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+
+        swap(arr, left+1,right-1);
 
 
-    // }
+
+    }
 }
