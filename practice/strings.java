0e9090f6c63406b6e9b09eabbfe76c5e59d1fe93
Ahamid047 <ahamid234567890@gmail.com>
2025-12-19 22:06:10 +0530

    first commit

---
+++ practice/strings.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class strings {
+    public static void main(String args[]){
+        String firstName = "Tony";
+           String secondName = "Stark";
+    
+    
+           String fullName = firstName + " " + secondName;
+           System.out.println(fullName.length());
+
+    }
+}
