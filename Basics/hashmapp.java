fe7a6c3486fd439e59f14c85b9b351f3cf2beee8
Ahamid047 <ahamid234567890@gmail.com>
2025-12-29 18:44:32 +0530

    new comment added

--- Basics/hashmapp.java
+++ Basics/hashmapp.java
@@ -5,9 +5,13 @@ public class hashmapp {
     public static void main(String[] args) {
         HashMap <Integer, String> map =new HashMap<>();
         map.put(1,"Abdul");
-        map.put(2,"hamid");
+        map.put(2,"hamid01");
+        
+        // System.out.println(map);
+        map.put(2,"hamid1");
+        System.out.println(map); // printed the last updated value
 
-        System.out.println(map);
+
         map.remove(1);
         if(map.containsKey(1)){
             System.out.println("present ");
