f86f99c5cfa0b91637499ce34d8777d9ca0b73c3
Ahamid047 <ahamid234567890@gmail.com>
2025-12-31 20:38:35 +0530

    Intersection of two array using hashset

---
+++ practice/intersection.java
@@ -0,0 +1,23 @@
+
+import java.util.HashSet;
+
+public class intersection {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        int count=0;
+        
+        int [] arr ={3,4,2,4,3};
+        int [] arr2= {8,6,3,4,1,2};
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]); 
+        }
+        for(int j =0;j<arr2.length;j++){
+            if(set.contains(arr2[j])){
+                count++;
+                set.remove(arr2[j]);
+            }
+        }
+        System.out.println(count);
+    }
+}
--- practice/unionarray.java
+++ practice/unionarray.java
@@ -3,8 +3,8 @@ import java.util.HashSet;
 
 public class unionarray {
     public static void main(String[] args) {
-        int[] arr ={1};
-        int[] arr2={2};
+        int[] arr ={1,2,4,5};
+        int[] arr2={2,7,9,8};
 
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
@@ -12,6 +12,10 @@ public class unionarray {
         }
          for (int i = 0; i < arr2.length; i++) {
             set.add(arr2[i]);
+        }
+        for (int i:set) {
+            System.out.println(i);
+            
         }
 
         System.out.println(set.size());
