8cbe2e6fa87061aeb803e227cb96300980797226
Ahamid047 <ahamid234567890@gmail.com>
2026-01-03 12:39:24 +0530

    Printing all the terms of the fibonacci

--- dp/fibo.java
+++ dp/fibo.java
@@ -1,16 +1,42 @@
+
 package dp;
-public class fibo{
-    public int fibonacci(int n){
-        if(n==0)return 0;
-        if(n==1)return 1;
 
-        return fibonacci(n-1)+fibonacci(n-2);
+public class fibo {
 
+    public int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
     public static void main(String[] args) {
         fibo obj = new fibo();
-        int n = obj.fibonacci(8);
-        System.out.println(n);
+        int n = 7;
 
+        for (int i = 0; i < n; i++) {
+            System.out.print(obj.fibonacci(i) + " ");
+        }
     }
 }
+
+
+
+
+
+// package dp;
+// public class fibo{
+//     public int fibonacci(int n){
+//         if(n==0)return 0;
+//         if(n==1)return 1;
+
+//         return fibonacci(n-1)+fibonacci(n-2);
+
+//     }
+//     public static void main(String[] args) {
+//         fibo obj = new fibo();
+//         int n = obj.fibonacci(6);
+//         System.out.println(n);
+
+//     }
+// }
