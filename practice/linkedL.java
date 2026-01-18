6311dc2189369e611223a2cf0449b9cc7a3f5e44
Ahamid047 <ahamid234567890@gmail.com>
2026-01-18 20:35:59 +0530

    Addign the node at the first place

--- practice/linkedL.java
+++ practice/linkedL.java
@@ -1,19 +1,45 @@
+package practice;
+
 public class linkedL {
-    class Node{
+
+    class Node {
+
         String data;
         Node next;
 
         //constructor
-        Node(String data){
-        this.data = data;
-        this.next=null;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+    Node head;
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+
+    }
+    public void printList() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
     }
 
-    
     public static void main(String[] args) {
         //Object
         linkedL list = new linkedL();
-        
+        list.addFirst("May");
+        list.addFirst("I");
+        list.printList();
+
     }
 }
