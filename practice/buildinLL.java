afff1d9f61d2fac06ed18986999f27e89dccbffa
Ahamid047 <ahamid234567890@gmail.com>
2026-01-18 22:30:30 +0530

    traversing the ll

--- practice/buildinLL.java
+++ practice/buildinLL.java
@@ -5,10 +5,15 @@ import java.util.LinkedList;
 public class buildinLL {
     public static void main(String[] args) {
         LinkedList <String> list = new LinkedList<>();
-        list.addFirst("Abdul");
+        list.addFirst("am");
         list.addFirst("I");
-        list.addLast("don");
+        list.addLast("Abdul");
         System.out.println(list);
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"->");
+        }
 
     }
 }
